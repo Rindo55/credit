72d1891dfaeb17d50de87e10be36d88c0944d00e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-09-28 20:25:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,10 @@
-from pyrogram import Client, idle, filters, enums
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from pyrogram import Client, filters
 import datetime
 import secrets
-import requests
-import time
 import asyncio
 from pymongo import MongoClient
 
+# Initialize the Pyrogram client
 app = Client(
     'my_bot',
     api_id=10247139,
@@ -18,38 +16,36 @@ app = Client(
 client = MongoClient('mongodb+srv://anime:2004@cluster0.ghzkqob.mongodb.net/?retryWrites=true&w=majority')
 db = client['user_tokens']
 db.user_tokens.create_index([("user_id", 1)], unique=True)
-async def is_user_authorized(user_id, usr_txt):
+
+async def is_user_authorized(user_id):
     # Check if the user_id exists in the user_tokens collection
-    user_token = db.user_tokens.find_one({"user_id": user_id, "token": usr_txt})
+    user_token = db.user_tokens.find_one({"user_id": user_id})
     if user_token:
         expiration_time = user_token["expiration_time"]
-        return expiration_time > asyncio.get_event_loop().time()
+        return expiration_time > datetime.datetime.utcnow()
     return False
 
 async def delete_expired_tokens():
     while True:
-        current_time = asyncio.get_event_loop().time()
+        current_time = datetime.datetime.utcnow()
         expired_tokens = db.user_tokens.find({"expiration_time": {"$lte": current_time}})
         for token in expired_tokens:
             db.user_tokens.delete_one({"_id": token["_id"]})
         await asyncio.sleep(30)
-        
-@app.on_message(filters.private & (filters.command("start", prefixes="/") | filters.command("help", prefixes="/")))
-async def handle_start_help_command(bot, cmd: Message):
+
+@app.on_message(filters.private & filters.command("start"))
+async def handle_start_command(bot, cmd):
     user_id = cmd.from_user.id
-    usr_cmd = cmd.text.split("_", 1)[-1]
-    usr_txt = usr_cmd.split("_")[-1]
-    if await is_user_authorized(user_id, usr_txt):
+    if await is_user_authorized(user_id):
         await cmd.reply("You are authorized to use the bot.")
-        
     else:
         # Generate a new token and set its expiration time to 24 hours from now
-        expiration_time = asyncio.get_event_loop().time() + 60
+        expiration_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=1)
         # Store the token in MongoDB
         enc = secrets.token_hex(nbytes=16)
         db.user_tokens.insert_one({"user_id": user_id, "token": enc, "expiration_time": expiration_time})
         await cmd.reply("You have been authorized for 24 hours. Please use this link to start: https://t.me/anime_data_bot?start=" + enc)
+
 if __name__ == '__main__':
     asyncio.ensure_future(delete_expired_tokens())
     app.run()
-
